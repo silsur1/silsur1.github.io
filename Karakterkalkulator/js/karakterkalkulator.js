e5df51b1706b03814262e2e1ce48efc8a763c452
silsur1 <116543947+silsur1@users.noreply.github.com>
2023-01-02 09:38:42 +0100

    Add files via upload

---
+++ Karakterkalkulator/js/karakterkalkulator.js
@@ -0,0 +1,15 @@
+let Karakter = prompt("vennligst tast in prøveresultatet dit")
+
+if (Karakter >= 90){
+    console.log("du fikk Karakteren 6");
+} else if (Karakter >= 80){
+    console.log("du fikk Karakteren 5");
+} else if (Karakter >= 70){
+    console.log("du fikk Karakteren 4");
+} else if (Karakter >= 60){
+    console.log("du fikk Karakteren 3");
+} else if (Karakter >= 50){
+    console.log("du fikk Karakteren 2");
+} else {
+    console.log("du bestod ikke")
+}
